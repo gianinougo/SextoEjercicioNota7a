efa1db80a96cd6ded1e4012a191fff8ac02d1ef0
Ugo <gianinougo@gmail.com>
2022-02-24 14:30:15 +0100

    Agregar archivos de proyecto.

---
+++ SextoEjercicioNota7a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SextoEjercicioNota7a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
